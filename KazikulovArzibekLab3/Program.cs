a0c875be185841d773a95f2e99663413a0031249
Alexander <alexxdaas@gmail.com>
2020-05-13 13:38:07 +0500

    add normal color pool

--- KazikulovArzibekLab3/Program.cs
+++ KazikulovArzibekLab3/Program.cs
@@ -1,26 +1,32 @@
 ﻿using System;
 using System.Drawing;
+using System.Linq;
 
 namespace KazikulovArzibekLab3
 {
     internal class Program
     {
+        
+        private static Color[] colors = new Color[] {Color.Brown, Color.Aqua, Color.Crimson, Color.Gold, Color.Green, Color.Indigo, };
+        
         public static void Main(string[] args)
         {
             var arguments = ArgumentProvider.ReadArguments();
             var polygons = BentliOtmanSolver.Solve(arguments.Points);
             var bitmap = new Bitmap(arguments.Width, arguments.Height);
+            
+         
 
             using (var graphics = Graphics.FromImage(bitmap))
             {
                 var random = new Random();
-                foreach (var polygon in polygons)
+                for(int polygonNumber = 0; polygonNumber < polygons.Count; polygonNumber++)
                 {
-                    var pen = new Pen(Color.FromArgb(random.Next(255),random.Next(255),random.Next(255)),2);
-                    for (int i = 0; i < polygon.Count; i++)
+                    var pen = new Pen(colors[polygonNumber % colors.Length] ,2);
+                    for (int i = 0; i < polygons[polygonNumber].Count; i++)
                     {
-                        var p1 = ToScreenCords(polygon[i],arguments.Width,arguments.Height);
-                        var p2 = ToScreenCords(polygon[(i + 1) % (polygon.Count)],arguments.Width,arguments.Height);
+                        var p1 = ToScreenCords(polygons[polygonNumber][i],arguments.Width,arguments.Height);
+                        var p2 = ToScreenCords(polygons[polygonNumber][(i + 1) % (polygons[polygonNumber].Count)],arguments.Width,arguments.Height);
                         graphics.DrawLine(pen,p1,p2);
                     }
                 }
@@ -30,7 +36,7 @@ namespace KazikulovArzibekLab3
             bitmap.Save(arguments.FileName + ".bmp");
         }
 
-        public static PointF ToScreenCords(PointF point, int width, int height)
+        private static PointF ToScreenCords(PointF point, int width, int height)
         {
             return new PointF(point.X + width / 2, (-point.Y) + height / 2);
         }
